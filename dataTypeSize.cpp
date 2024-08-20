1a906e2314a3a7163668e50715c799e7df6986d6
sarasankit <shah.alg@gmail.com>
2024-08-20 18:14:28 +0530

    Display primitive data type sizes

---
+++ dataTypeSize.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "The size of bool   data type is " << sizeof(bool) << endl;
+    cout << "The size of char   data type is " << sizeof(char) << endl;
+    cout << "The size of int    data type is " << sizeof(int) << endl;
+    cout << "The size of float  data type is " << sizeof(float) << endl;
+    cout << "The size of double data type is " << sizeof(double) << endl;
+    return 0;
+}
